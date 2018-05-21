a28e8af76b65753a6805cb96157f1bfdfa118f29
Luca Baroncini <baroncini@netseven.it>
2018-05-21 17:39:10 +0200

    fixed shortcode brenta multiverso

--- wp-content/themes/brenta/functions.php
+++ wp-content/themes/brenta/functions.php
@@ -954,7 +954,8 @@ function br_single_category($atts) {
   // Attributes
   extract( shortcode_atts(
       array(
-        'id' => 0
+        'id' => 0,
+        'last_days' => 'false'
       ), $atts )
   );
 
@@ -976,7 +977,7 @@ function br_single_category($atts) {
 //             DISPLAY CATEGORY FILES            //
 //***********************************************//
 
-function br_display_catfiles($catslug, $catname = NULL, $personal = false, $registered = false) {
+function br_display_catfiles($catslug, $catname = NULL, $last_days, $personal = false, $registered = false) {
 
   $html = '';
 
@@ -994,15 +995,9 @@ function br_display_catfiles($catslug, $catname = NULL, $personal = false, $regi
       'meta_key' => 'mv_user',
       'meta_value' => $mv_logged,
       'meta_compare' => '==',
-      'orderby' => 'date',
+      'orderby' => 'slug',
       'order' => 'DESC',
       'posts_per_page' => -1,
-      'date_query' => array(
-        array(
-          'after' => '-15 days',
-          'column' => 'post_date',
-        ),
-      ),
       'tax_query' => array(
         array(
           'taxonomy' => 'multiverso-categories',
@@ -1037,15 +1032,9 @@ function br_display_catfiles($catslug, $catname = NULL, $personal = false, $regi
           'compare' => '=='
         ),
       ),
-      'orderby' => 'date',
+      'orderby' => 'slug',
       'order' => 'DESC',
       'posts_per_page' => -1,
-      'date_query' => array(
-        array(
-          'after' => '-15 days',
-          'column' => 'post_date',
-        ),
-      ),
       'tax_query' => array(
         array(
           'taxonomy' => 'multiverso-categories',
@@ -1062,15 +1051,9 @@ function br_display_catfiles($catslug, $catname = NULL, $personal = false, $regi
     $args = array(
       'post_type'	 =>	'multiverso',
       'post_status' => 'publish',
-      'orderby' => 'date',
+      'orderby' => 'slug',
       'order' => 'DESC',
       'posts_per_page' => -1,
-      'date_query' => array(
-        array(
-          'after' => '-15 days',
-          'column' => 'post_date',
-        ),
-      ),
       'tax_query' => array(
         array(
           'taxonomy' => 'multiverso-categories',
@@ -1083,6 +1066,14 @@ function br_display_catfiles($catslug, $catname = NULL, $personal = false, $regi
 
   }
 
+  if ($last_days == 'true') {
+    $args['date_query'] = array(
+      array(
+        'after' => '-15 days',
+        'column' => 'post_date',
+      ),
+    );
+  }
 
   // Start Query
   $files = new WP_Query( $args  );
@@ -1104,7 +1095,7 @@ function br_display_catfiles($catslug, $catname = NULL, $personal = false, $regi
         $fcount++;
 
         // Display the File
-        $html .= mv_file_details_html( get_the_ID() );
+        $html .= br_file_details_html( get_the_ID() );
 
       }
 
@@ -1120,6 +1111,187 @@ function br_display_catfiles($catslug, $catname = NULL, $personal = false, $regi
 
     }
 
+
+  }
+
+  // Return HTML
+  return $html;
+
+}
+
+
+//***********************************************//
+//               DISPLAY FILE HTML               //
+//***********************************************//
+
+// File Details (HTML)
+function br_file_details_html( $fileID ) {
+
+  $html = '';
+
+  $mv_access = get_post_meta($fileID, 'mv_access', true);
+  $mv_user = get_post_meta($fileID, 'mv_user', true);
+  $mv_file_check = get_post_meta($fileID, 'mv_file', true);
+
+
+  if (!empty($mv_file_check)) {
+
+
+    // Switch right icon
+    switch ($mv_file_check['type']) {
+
+      case 'application/pdf':
+        $icon_class = 'file-pdf';
+        break;
+
+      case 'text/plain':
+        $icon_class = 'file-txt';
+        break;
+
+      case 'application/zip':
+        $icon_class = 'file-zip';
+        break;
+
+      case 'application/rar':
+        $icon_class = 'file-rar';
+        break;
+
+      case 'application/msword':
+        $icon_class = 'file-doc';
+        break;
+
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        $icon_class = 'file-doc';
+        break;
+
+      case 'application/vnd.ms-excel':
+        $icon_class = 'file-xls';
+        break;
+
+      case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+        $icon_class = 'file-xls';
+        break;
+
+      case 'application/vnd.ms-powerpoint':
+        $icon_class = 'file-ppt';
+        break;
+
+      case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+        $icon_class = 'file-ppt';
+        break;
+
+      case 'image/gif':
+        $icon_class = 'file-gif';
+        break;
+
+      case 'image/png':
+        $icon_class = 'file-png';
+        break;
+
+      case 'image/jpeg':
+        $icon_class = 'file-jpg';
+        break;
+
+      default:
+        $icon_class = 'file-others';
+        break;
+    }
+
+    // Filename
+    $filename_ar = explode('/', $mv_file_check['file']);
+
+    if(!empty($filename_ar[1])){
+
+      $filename = $filename_ar[1];
+
+      if (get_option('mv_disable_downloader') == 1) {
+        $filedownload = WP_CONTENT_URL.'/'.WP_MULTIVERSO_UPLOAD_FOLDER.'/'.$filename_ar[0].'/'.$filename_ar[1];
+      }else{
+        $filedownload = '?upf=dl&id='.$fileID;
+      }
+
+
+
+    }else{
+      $filename = $mv_file_check['file'];
+      $filedownload = $mv_file_check['url'];
+    }
+
+    $html .= '
+	
+	<div class="file-details-wrapper">
+        <div class="file-details">     
+    
+
+        <div class="file-data '.$icon_class.'">';
+
+    if (get_option('mv_single_theme') == 1) {
+      $html .= '<div class="file-name"><a href="'.get_the_permalink($fileID).'" title="'.__('View details', 'mvafsp').'">'.get_the_title($fileID).'</a> 
+				 (<a href="'.$filedownload.'" target="_blank" title="'.__('Download file', 'mvafsp').'">'.$filename.'</a>)</div>';
+    }else{
+      $html .= '<div class="file-name">'.__('File', 'mvafsp').' <a href="'.$filedownload.'" target="_blank" title="'.__('Download file', 'mvafsp').'">'.$filename.'</a></div>';
+    }
+    $html .= '<div class="br-file-excerpt">'. get_the_excerpt($fileID).'</div>';
+    $html .= '<ul class="file-data-list">
+            	<li class="file-owner"><i class="mvico-user3" title="'.__('Owner', 'mvafsp').'"></i>'.ucfirst($mv_user).'</li>
+                <li class="file-publish"><i class="mvico-calendar"></i>'.get_post_time( 'F j, Y', false,  $fileID ).'</li>
+                <li class="file-access file-'.$mv_access.'"><i class="mvico-eye"></i>'.ucfirst($mv_access).'</li>
+            </ul>
+        </div>
+		
+		<div class="file-dw-button">
+        <a class="mv-btn mv-btn-success" href="'.$filedownload.'" target="_blank" title="'.__('Download file', 'mvafsp').'">'.__('Download', 'mvafsp').'</a>
+        </div>';
+
+    if (get_option('mv_single_theme') == 1) {
+      $html .= '<div class="file-dw-button">
+        			  <a class="mv-btn mv-btn-success" href="'.get_the_permalink($fileID).'" title="'.__('Details', 'mvafsp').'">'.__('Details', 'mvafsp').'</a>
+        			  </div>';
+    }
+
+    $html .= '<div class="mv-clear"></div>
+		
+        </div>
+        </div>';
+
+
+
+  }else{
+
+    $html .= '
+	
+	<div class="file-details-wrapper">
+        <div class="file-details">     
+    
+
+        <div class="file-data file-others">';
+
+    if (get_option('mv_single_theme') == 1) {
+      $html .= '<div class="file-name"><a href="'.get_the_permalink($fileID).'" title="'.__('View details', 'mvafsp').'">'.get_the_title($fileID).'</a></div>';
+    }else{
+      $html .= __('File: none', 'mvafsp');
+    }
+    $html .= '<ul class="file-data-list">
+            	<li class="file-owner"><i class="mvico-user3" title="'.__('Owner', 'mvafsp').'"></i>'.ucfirst($mv_user).'</li>
+                <li class="file-publish"><i class="mvico-calendar"></i>'.get_post_time( 'F j, Y', false,  $fileID ).'</li>
+                <li class="file-access file-'.$mv_access.'"><i class="mvico-eye"></i>'.ucfirst($mv_access).'</li>
+            </ul>
+        </div>
+		
+		<div class="file-dw-button disabled" title="'.__('No file uploaded yet', 'mvafsp').'">
+        <span class="mv-btn mv-btn-success">'.__('Download', 'mvafsp').'</span>
+        </div>';
+
+    if (get_option('mv_single_theme') == 1) {
+      $html .= '<div class="file-dw-button">
+			<a class="mv-btn mv-btn-success" href="'.get_the_permalink($fileID).'" title="'.__('Details', 'mvafsp').'">'.__('Details', 'mvafsp').'</a>
+			</div>';
+    }
+
+    $html .= '<div class="mv-clear"></div>
+		
+        </div>
+        </div>';
 
   }
 
--- wp-content/themes/brenta/include/brenta-doc-category.php
+++ wp-content/themes/brenta/include/brenta-doc-category.php
@@ -60,7 +60,7 @@ if(isset($_GET['catid'])) {
   //$html .= mv_display_subcategories($cat->term_id);
 
   // Files
-  $html .= br_display_catfiles($cat->slug, $cat->name);
+  $html .= br_display_catfiles($cat->slug, $cat->name, $last_days);
 
   $html .= '</div>';
 
